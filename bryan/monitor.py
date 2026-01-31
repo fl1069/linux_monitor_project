9d43187c8e376b90e43b4c941edfc50ef4e3b26c
fl1069 <chongbryan1069@gmail.com>
2026-02-01 02:06:47 +0800

    backup

--- bryan/monitor.py
+++ bryan/monitor.py
@@ -1,71 +1,34 @@
-# get system info (cpu, memory, disk, process)
-import psutil
+import psutil #toolsssssssssssssssssssssssssssss
 
-# used for timer
-import time
+#log file loaction
+log_file = "report/system_log.txt"
 
-# used to write csv file
-import csv
+#cpu metrics
+cpu_usage_percentage = psutil.cpu_percent(percpu=True)
+cpu_load_average1_5_15 = psutil.getloadavg()
+running_process =[]
+for p in psutil.process_iter():
+          if p.status() == psutil.STATUS_RUNNING:
+             running_process.append(p)
+running_process_count = len(running_process)
+#memory metrics
+memory = psutil.virtual_memory() #all information of memory
+memory_total = memory.total
+memory_used = memory.used
+memory_available = memory.available
+memory_usage_percentage = memory.percent
+#disk metrics
+disk = psutil.disk_usage('/') #this include everthing and need to choose path(root)
+#system uptime(using proc this time 11111111111111)(a file in root record memory and uptime info)
+with open('/proc/uptime','r') as f:
+    uptime_seconds,idle_second = map(float,f.read().split())
+#active process
+total_process = len(psutil.pids())
+sleeping_process =[]
+for p in psutil.process_iter():
+          if p.status() == psutil.STATUS_SLEEPING:
+             sleeping_process.append(p)
+sleeping_process_count = len(sleeping_process)
 
-# used to get current time
-from datetime import datetime
-
-# csv file loaction
-log_file = "reports/system_log.csv"
-
-# check if csv file exist
-try:
-    f = open(log_file, "r")   # try open file
-    f.close()
-except:
-    # create csv file if not exist
-    f = open(log_file, "w", newline="")
-    writer = csv.writer(f)
-
-    # write colum names
-    writer.writerow([
-        "time",
-        "cpu",
-        "memory",
-        "disk",
-        "process_total"
-    ])
-
-    f.close()
-
-while True:
-    # get current time in string
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-
-    # get cpu usage
-    cpu = psutil.cpu_percent()
-
-    # get memory usage
-    memory = psutil.virtual_memory().percent
-
-    # get disk usage
-    disk = psutil.disk_usage("/").percent
-
-    # get total process count
-    process_total = len(psutil.pids())
-
-    # open csv file to add new data
-    f = open(log_file, "a", newline="")
-    writer = csv.writer(f)
-
-    # write one row into csv
-    writer.writerow([
-        current_time,
-        cpu,
-        memory,
-        disk,
-        process_total
-    ])
-
-    f.close()
-
-    # show record time in terminal
-    print("recorded at", current_time)
-
-    # wait 10s
-    time.sleep(10)
+with open(log_file,'w') as f:
+     f.write(str(cpu_usage_percentage))
